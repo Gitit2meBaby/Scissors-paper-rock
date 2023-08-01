a42e7e2442de4fbbe2f4be13a304fccebe3e67eb
Gitit2meBaby <thisworldofdans@gmail.com>
2023-08-01 11:58:52 +0700

    added the scoring functions and 5 round limit with play again option

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // play mad tunes
-const musicButton = document.querySelector('.tunes');
+const musicButton = document.querySelector('#tunes-btn');
 musicButton.addEventListener('click', playMusic);
 
 // Play music function
@@ -8,6 +8,24 @@ function playMusic() {
   music.play();
 }
 
+// get some scoring in this dog
+let userScore = 0;
+let computerScore = 0;
+let gamesPlayed = 0
+
+function updateScores() {
+  const userScoreDisplay = document.querySelector("#userScore")
+  const computerScoreDisplay = document.querySelector("#computerScore")
+
+  userScoreDisplay.textContent = userScore
+  computerScoreDisplay.textContent = computerScore
+}
+
+// function resetScores() {
+//   userScore = 0;
+//   computerScore = 0;
+//   updateScores();
+// }
 
 // Get user input choice
 const rock = document.querySelector('.rock');
@@ -34,25 +52,24 @@ const drawSound = new Audio('draw.wav');
 
 // Start the game
 function startGame(event) {
-    const userChoice = event.target.value;
-    let choiceDisplay;
-  
-    switch (userChoice) {
-      case '0':
-        choiceDisplay = 'rock..';
-        break;
-      case '1':
-        choiceDisplay = 'paper..';
-        break;
-      case '2':
-        choiceDisplay = 'scissors..';
-        break;
-      default:
-        choiceDisplay = '';
-        break;
-    }
-  
-    document.querySelector('.user-choice').innerHTML = choiceDisplay;
+  const userChoice = event.target.value;
+  let choiceDisplay;
+  switch (userChoice) {
+    case '0':
+      choiceDisplay = 'rock..';
+      break;
+    case '1':
+      choiceDisplay = 'paper..';
+      break;
+    case '2':
+      choiceDisplay = 'scissors..';
+      break;
+    default:
+      choiceDisplay = '';
+      break;
+  }
+
+  document.querySelector('.user-choice').innerHTML = choiceDisplay;
   // Play the initial audio
   const initialAudio = new Audio('janken.mp3');
   initialAudio.play();
@@ -82,47 +99,104 @@ function continueGame(userChoice) {
   setTimeout(() => {
     // Compare userChoice and computerChoice
     if (parseInt(userChoice) === computerChoice) {
-      document.querySelector('.result').innerHTML = "It's a draw";
+      // document.querySelector('.result').innerHTML = "It's a draw";
       document.querySelector('.draw-image').style.display = 'flex';
       playAudio(drawSound);
+      gamesPlayed++;
     } else if (userChoice === '1' && computerChoice === 0) {
-      document.querySelector('.result').innerHTML = 'You Win!';
+      // document.querySelector('.result').innerHTML = 'You Win!';
       document.querySelector('.win-image').style.display = 'flex';
       playAudio(winSound);
+      userScore++
+      gamesPlayed++
     } else if (userChoice === '1' && computerChoice === 2) {
-        document.querySelector('.result').innerHTML = 'You Lose!';
-        document.querySelector('.lost-image').style.display = 'flex';
-        playAudio(lossSound);
-    } else if (userChoice === '2' && computerChoice === 0) {
-      document.querySelector('.result').innerHTML = 'You Lose!';
+      // document.querySelector('.result').innerHTML = 'You Lose!';
       document.querySelector('.lost-image').style.display = 'flex';
       playAudio(lossSound);
+      computerScore++
+      gamesPlayed++
+    } else if (userChoice === '2' && computerChoice === 0) {
+      // document.querySelector('.result').innerHTML = 'You Lose!';
+      document.querySelector('.lost-image').style.display = 'flex';
+      playAudio(lossSound);
+      computerScore++
+      gamesPlayed++
     } else if (userChoice === '2' && computerChoice === 1) {
-      document.querySelector('.result').innerHTML = 'You Win!';
+      // document.querySelector('.result').innerHTML = 'You Win!';
       document.querySelector('.win-image').style.display = 'flex';
       playAudio(winSound);
+      userScore++
+      gamesPlayed++
     } else if (userChoice === '0' && computerChoice === 1) {
-        document.querySelector('.result').innerHTML = 'You Lose!';
-        document.querySelector('.lost-image').style.display = 'flex';
-        playAudio(lossSound);
+      // document.querySelector('.result').innerHTML = 'You Lose!';
+      document.querySelector('.lost-image').style.display = 'flex';
+      playAudio(lossSound);
+      computerScore++
+      gamesPlayed++
     } else if (userChoice === '0' && computerChoice === 2) {
-      document.querySelector('.result').innerHTML = 'You Win!';
+      // document.querySelector('.result').innerHTML = 'You Win!';
       document.querySelector('.win-image').style.display = 'flex';
       playAudio(winSound);
-   
+      userScore++
+      gamesPlayed++
     }
+
+    updateScores()
+    console.log(gamesPlayed)
+
     // Clear the choice display
     setTimeout(() => {
-        document.querySelector('.user-choice').innerHTML = '';
-        document.querySelector('.computer-choice').innerHTML = '';
-        document.querySelector('.lost-image').style.display = 'none';
-        document.querySelector('.win-image').style.display = 'none';
-        document.querySelector('.draw-image').style.display = 'none';
-      }, 3000);
-  }, 500); // Adjust the delay (in milliseconds) as per your preference
-}
+      document.querySelector('.user-choice').innerHTML = '';
+      document.querySelector('.computer-choice').innerHTML = '';
+      document.querySelector('.lost-image').style.display = 'none';
+      document.querySelector('.win-image').style.display = 'none';
+      document.querySelector('.draw-image').style.display = 'none';
+    }, 3000);
 
+    isEndGame()
+
+  }, 500); // Adjust the delay (in milliseconds) as per your preference
+
+
+
+  function isEndGame() {
+    const endGameContainer = document.querySelector('#gameOverContainer');
+    const youLose = document.querySelector("#youLose");
+    const youWin = document.querySelector("#youWin");
+    const draw = document.querySelector("#draw");
+    const gameboard = document.querySelector(".gameboard");
+    const playAgainBtn = document.querySelector("#playAgain")
+
+    if (gamesPlayed === 5) {
+      gameboard.style.display = 'none';
+      endGameContainer.style.display = 'flex';
+      if (computerScore > userScore) {
+        youLose.style.display = 'flex';
+      } else if (computerScore == userScore) {
+        draw.style.display = 'flex'
+      } else {
+        youWin.style.display = 'flex';
+      }
+    }
+    playAgainBtn.addEventListener('click', () => {
+      gameboard.style.display = 'flex';
+      endGameContainer.style.display = 'none';
+      userScore = 0
+      computerScore = 0
+      gamesPlayed = 0
+      updateScores()
+      startGame(event)
+    })
+  }
+
+}
 // Play an audio track
 function playAudio(audio) {
   audio.play();
 }
+
+
+// limit it to 5 games, display the score and a winner
+console.log(userScore)
+console.log(computerScore)
+console.log(gamesPlayed)
